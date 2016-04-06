64a1ac5a5ad12a57a4d7db94494e382b00f48c86
Rooshy <ru_sha_nia@yahoo.ca>
2016-04-05 22:32:51 -0500

    Reseting DB with migrations

--- db/migrate/20160405180356_create_juices.rb
+++ db/migrate/20160405180356_create_juices.rb
@@ -2,6 +2,8 @@ class CreateJuices < ActiveRecord::Migration
   def change
     create_table :juices do |t|
       t.string :name
+      t.integer :juice_id
+      t.integer :health_issue_id
 
       t.timestamps null: false
     end
