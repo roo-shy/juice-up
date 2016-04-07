daa798a71b0e5c36a540be95d80991a10bd1062e
Rooshy <ru_sha_nia@yahoo.ca>
2016-04-06 22:43:20 -0500

    editing seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,14 +4,17 @@ Blueberries contain pectin, vitamin C, potassium, and significant amounts of tan
 j3 = Juice.create name: "Pomegranate", description: "Very rich in tannins and flavonoids. Pomegranates hold promise in helping reduce the risk of prostate cancer due to their lycopene and other phytonutrient content. They are the 3rd highest fruit source of antioxidants. Pomegranates have also been used medicinally to treat tapeworm and diarrhea since Roman times, according to the British Journal of Medicine"
 j4 = Juice.create name: "Mango/Peach", description: "Excellent source of vitamin A and C, which is very beneficial to maintaining a strong immune system. Has been found to protect against colon, breast, leukemia and prostate cancers and is a good source of potassium, which is important in assisting to control heart rate and blood pressure. Mango contains a great deal of flavonoids like beta-carotene, alpha-carotene, and beta-cryptoxanthin. Excellent source of vitamin A and potassium"
 j5 = Juice.create name: "Pineapple", description: "Great for improving digestion. A good source of potassium and has vitamin C and iron. Contains anti-inflammatory compounds such as bromelain"
-j6 = Juice.create name: "Orange", description: "Excellent source of vitamin C. Vitamin C helps heal cuts and wounds and keep teeth and gums healthy"
+j6 = Juice.create name: "Simply Orange", description: "Excellent source of vitamin C. Vitamin C helps heal cuts and wounds and keep teeth and gums healthy"
 j7 = Juice.create name: "Takin’ Care Of Business", description: "Numerous studies have shown that beets can help oxygenate blood and enhance exercise performance. Carrots are the ABCs of health they help protect the body from cancer, cardiac disease, and cataract and macular degeneration.
 Kale is a wonder vegetable! Kale is low calorie and promotes eye and skin health. A great low calorie food. It contains high levels of silicon in celery lend it to helping to strengthen joints, bones, arteries, and connective tissues. High in minerals, including this veggie as part of a healthy diet may help promote healthy blood pressure"
 j8 = Juice.create name: "Kickstart My Heart", description: "Fresh acai, blueberry, pomegranate, apple and fresh, natural mint are blended together to create this berry inspired cleanse juice. Acai and blueberries, high in antioxidants help improve gastro-intestinal function"
 j9 = Juice.create name: "Lovin’ Touchin’ Squeezin", description: "Fresh, natural pineapple, coconut water and freshly pressed wheatgrass are combined to create this fruity and healthy cleanse juice. Wheatgrass contains a hefty nutritional punch, containing high amounts of vitamins. Coconut is rich in naturally occurring bioactive enzymes which help the digestion system and metabolism"
 j10 = Juice.create name: "Cashew Milk", description: "Freshly blended and pressed cashews, combined with agave and cinnamon create a delicious and healthy nut milk cleanse. Cashews contain over 38% of your daily intake of copper, beneficial for various bodily functions, as well as containing an abundance of magnesium and calcium. A cholesterol-free alternative to other milks, this is a delicious and healthy cleanse juice"
 
-digestion = HealthIssue.create name: "Digestion"
+digestion = HealthIssue.create name: "Digestion, Stomach"
+# file  = File.open(File.join(Rails.root,'app/assets/images/medical-1.png'))
+# image = @digestion.build_image(filename: file)
+# image.save
 heart = HealthIssue.create name: "High Blood Pressure"
 eyes = HealthIssue.create name: "Eyes"
 bones = HealthIssue.create name: "Bones"
@@ -20,14 +23,15 @@ teeth = HealthIssue.create name: "Teeth"
 gums = HealthIssue.create name: "Gums"
 blood = HealthIssue.create name: "Diabetes"
 weight = HealthIssue.create name: "Weight Loss"
-inflammation = HealthIssue.create name: "Inflammation"
+respiratory = HealthIssue.create name: "Respiratory System"
+inflammation = HealthIssue.create name: "inflammation"
 
 Remedy.create juice_id: j1.id,
                        health_issue_id: teeth.id
 Remedy.create juice_id: j2.id,
                        health_issue_id: blood.id
-Remedy.create juice_id: j3.id,
-                      health_issue_id: inflammation.id
+Remedy.create juice_id: j6.id,
+                      health_issue_id: respiratory.id
 Remedy.create juice_id: j4.id,
                       health_issue_id: blood.id
 Remedy.create juice_id: j5.id,
@@ -42,8 +46,8 @@ Remedy.create juice_id: j9.id,
                       health_issue_id: digestion.id
 Remedy.create juice_id: j10.id,
                       health_issue_id: weight.id
-
-
+Remedy.create juice_id: j6.id,
+                      health_issue_id: respiratory.id
 # Remedy.create juice: j7, weight: "Weight", remedy: "Weight Loss"
 # Remedy.create juice: j1, teeth: "Teeth", remedy: "Teeth Health"
 
